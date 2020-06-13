2fa1dcf8ec4761eebc82660e8ea9653b9edd6a58
Hofls <pleshakov@proitr.ru>
2020-06-13 17:35:48 +0500

    Machine learning (cats-dogs)

--- machine-learning/image-recognition/cats-dogs/README.md
+++ machine-learning/image-recognition/cats-dogs/README.md
@@ -1,9 +1,11 @@
 How to use:
 * Train and save model
-    *
-* 
+    * Download [archive](https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip)
+    * Unzip archive into `train-model` folder
+    * Run `use_trained_model.py`
+    * Model should be trained and saved into `saved_models` folder
 * Load and use model
-    * Copy saved model from `train-model/` to `use-trained-model/trained_model` 
+    * Copy saved model from `train-model/saved_models` to `use-trained-model/trained_model` 
     * Save random images of dogs and cats from the internet to `use-trained-model/test_images`
     * Run `use_trained_model.py`
     * Model should identify all `.jpg` images in `test_images` folder
--- machine-learning/image-recognition/cats-dogs/use-trained-model/use_trained_model.py
+++ machine-learning/image-recognition/cats-dogs/use-trained-model/use_trained_model.py
@@ -1,3 +1,8 @@
+# Expectations:
+# Model was trained to identify 150x150 images of cats and dogs
+# This folder contains subfolder called 'test_images', with bunch of .jpg images
+# This folder contains subfolder called 'trained_model', with model exported in SavedModel format (contains assets, variables, saved_model.pb)
+
 import tensorflow as tf
 import glob, os
 import numpy as np
@@ -10,27 +15,20 @@ def identifyPicture(pic, loaded_model):
     #print(pix.shape)
     #print(pix[np.newaxis, ...].shape)
 
-    result = loaded_model.predict(pix[np.newaxis, ...]);
+    result = loaded_model.predict(pix[np.newaxis, ...])
     if (result > 0): # Result depends on activation function that is used during model training
         print(file + ' is a dog')
     else:
         print(file + ' is a cat')
 
+current_directory = os.path.dirname(os.path.realpath(__file__))
+images_directory = os.path.join(current_directory, "test_images")
+model_directory = os.path.join(current_directory, "trained_model")
 
-
-# Expectations:
-# Model was trained to identify 150x150 images of cats and dogs
-# This folder contains subfolder called 'test_images', with bunch of .jpg images
-# This folder contains subfolder called 'trained_model', with model exported in SavedModel format (contains assets, variables, saved_model.pb)
-
-currentDirectory = os.path.dirname(os.path.realpath(__file__))
-imagesDirectory = currentDirectory + "\\test_images"
-modelDirectory = currentDirectory + "\\trained_model"
-
-loaded_model = tf.keras.models.load_model(modelDirectory)
+loaded_model = tf.keras.models.load_model(model_directory)
 #loaded_model.summary() # Check its architecture
 
-os.chdir(imagesDirectory)
+os.chdir(images_directory)
 for file in glob.glob("*.jpg"):
-    pic = Image.open(imagesDirectory + "\\" + file)
+    pic = Image.open(file)
     identifyPicture(pic, loaded_model)
