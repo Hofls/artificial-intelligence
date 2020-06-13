6a8191add73e40ea2764fa3a034b1b44c2e73c62
Hofls <pleshakov@proitr.ru>
2020-06-14 00:50:19 +0500

    Automatically calculate number of images

--- machine-learning/image-recognition/cats-dogs/train-model/train_model.py
+++ machine-learning/image-recognition/cats-dogs/train-model/train_model.py
@@ -31,26 +31,9 @@ def showImages(images_arr):
 
 # Understand the data
 def getDirectoriesStats(train_dir, validation_dir):
-    train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
-    train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
-    validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
-    validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
+    total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+    total_val = sum([len(files) for r, d, files in os.walk(train_dir)])
 
-    num_cats_tr = len(os.listdir(train_cats_dir))
-    num_dogs_tr = len(os.listdir(train_dogs_dir))
-
-    num_cats_val = len(os.listdir(validation_cats_dir))
-    num_dogs_val = len(os.listdir(validation_dogs_dir))
-
-    total_train = num_cats_tr + num_dogs_tr
-    total_val = num_cats_val + num_dogs_val
-
-    print('total training cat images:', num_cats_tr)
-    print('total training dog images:', num_dogs_tr)
-
-    print('total validation cat images:', num_cats_val)
-    print('total validation dog images:', num_dogs_val)
-    print("--")
     print("Total training images:", total_train)
     print("Total validation images:", total_val)
 
