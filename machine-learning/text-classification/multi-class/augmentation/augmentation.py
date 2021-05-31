5f88a655850a15f2539a67e223ea1b271e19a60e
Hofls <hofls@ya.ru>
2021-05-31 21:07:02 +0500

    augmentation.py

--- machine-learning/text-classification/multi-class/augmentation/augmentation.py
+++ machine-learning/text-classification/multi-class/augmentation/augmentation.py
@@ -4,6 +4,18 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import re
 
+class TextAugmentation(tf.keras.utils.Sequence):
+
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return self.x, self.y
+
 def standardizeData(sentences):
   data = []
   for sentence in sentences:
@@ -14,8 +26,8 @@ def standardizeData(sentences):
 
 def getTrainingData():
   csv = pd.read_csv('training-set.csv', delimiter=';')
-  labels = data.to_numpy()[:, 0]
-  questions = data.to_numpy()[:, 1]
+  labels = csv.to_numpy()[:, 0]
+  questions = csv.to_numpy()[:, 1]
   return {
       "labels": np.asarray(labels).astype('float32'),
       "questions": standardizeData(questions)
@@ -31,7 +43,8 @@ def eachToNumbers(sentences):
   data = []
   for sentence in sentences:
     data.append(toNumbers(sentence))
-  return tf.keras.preprocessing.sequence.pad_sequences(convertedData, padding='post')
+  return tf.keras.preprocessing.sequence.pad_sequences(data, padding='post')
+
 
 
 rawData = getTrainingData()
@@ -50,9 +63,9 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(3) # unique labels count
  ])
 
-
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-history = model.fit(convertedData, labels, epochs=200, verbose=0)
+gener = TextAugmentation(convertedData, labels, 12)
+history = model.fit(gener, epochs=25, verbose=0)
 plt.plot(history.history['loss'])
 
 print(model.predict(convertedData))
