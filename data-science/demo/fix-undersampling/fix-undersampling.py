7da5239eb4b5470515369bdf25864248603f692b
Hofls <hofls@ya.ru>
2021-06-06 12:05:39 +0500

    fix-undersampling.py

---
+++ data-science/demo/fix-undersampling/fix-undersampling.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import csv
+
+def read_csv():
+    csv = pd.read_csv('data/original.csv', delimiter=';')
+    headers = list(csv.columns.values)
+    return {
+        "items": csv.to_numpy(),
+        "headers": headers
+    }
+
+def write_csv(items, headers):
+    with open("data/processed.csv", "w", newline="") as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerow(headers)
+        writer.writerows(items)
+
+def fix(items):
+    labels_count = get_labels_count(items)
+    max_count = get_max_count(labels_count)
+    new_items = []
+    for label in labels_count:
+        new_samples_count = max_count - labels_count[label]
+        samples = create_samples(items, label, new_samples_count)
+        if samples:
+            new_items.append(samples)
+    print(items[0])
+    print(new_items[0][0])
+    return items
+
+def create_samples(items, expected_label, new_samples_count):
+    if new_samples_count == 0:
+        return
+    actual_items = items_with_label(items, expected_label)
+    new_items = []
+    added_samples = 0
+    while added_samples < new_samples_count:
+        for item in actual_items:
+            new_items.append(item)
+            added_samples += 1
+    return new_items
+
+def items_with_label(items, expected_label):
+    with_label = []
+    for item in items:
+        label = item[0]
+        if label == expected_label:
+            with_label.append(item)
+    return with_label
+
+def get_labels_count(items):
+    count = {}
+    for item in items:
+        label = item[0]
+        if label in count:
+            count[label] = count[label] + 1
+        else:
+            count[label] = 1
+    return count
+
+def get_max_count(labels_count):
+    max_key = max(labels_count, key=labels_count.get)
+    return labels_count[max_key]
+
+csv_data = read_csv()
+items = fix(csv_data["items"])
+write_csv(items, csv_data["headers"])
